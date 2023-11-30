e9bee9634ffda9ae7d9de0ff1e5960009f9fbefa
1stKING <62467459+1stKING@users.noreply.github.com>
2023-11-30 23:42:18 +0000

    comm1

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python3
+## !/usr/bin/python3
 import random
 number = random.randint(-10, 10)
 
-# YOUR CODE HERE
 print("The number is", number)
 if number > 0:
     print("is positive")
